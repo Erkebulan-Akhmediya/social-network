e1377cafcf6e9010fa39f2ef0e49be9d6f3cdb42
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-04-07 19:45:56 +0500

    added get user by id

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,11 +1,31 @@
-import type { Request, Response} from 'express';
-import {/*addFriend,*/ getUsers, User} from "./user.service";
+import type {Request, Response} from 'express';
+import {getUsers, User} from "./user.service";
 
 export async function search(req: Request, res: Response): Promise<void> {
     try {
         const {username} = req.query;
         const users: User[] = await getUsers({username: String(username)});
         res.json(users)
+    } catch (e) {
+        console.error(e);
+        res.status(500).end();
+    }
+}
+
+export async function getById(req: Request, res: Response): Promise<void> {
+    try {
+        const {id: idParam} = req.params;
+        if (!idParam || Array.isArray(idParam)) {
+            res.status(400).send({error: 'Invalid user id'});
+            return
+        }
+        const id: number = parseInt(idParam)
+        const [user] = await getUsers({id})
+        if (!user) {
+            res.status(404).send({error: 'User not found'});
+            return
+        }
+        res.json(user)
     } catch (e) {
         console.error(e);
         res.status(500).end();
--- src/user.router.ts
+++ src/user.router.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import {search, } from "./user.controller";
+import {search, getById} from "./user.controller";
 import {authMiddleware} from "./auth.service";
 
 const router = Router();
 
 router.get('/search', authMiddleware, search);
+router.get('/:id', authMiddleware, getById)
 
 export default router;
